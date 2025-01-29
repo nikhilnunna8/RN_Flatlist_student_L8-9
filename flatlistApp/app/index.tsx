5c570eca184bd1b5a0eb5aa46458bbd898901223
nunna8021 <nikhil.nunna9@gmail.com>
2025-01-29 08:18:49 -0600

    Added the conditional coloring
    Learned how to work with flatList and conditional coloring

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -4,11 +4,30 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import {useState} from 'react';
 
 export default function Index() {
+  const [selectedId, setSelectedId] = useState<string>("1") //makes the first item selected by default
+  type dataType = {
+    id: string; // unique identifier
+    title: string;
+  }
+  const DATA: dataType[] = [
+    {id: '1', title: "First"},
+    {id: '2', title: "Second"},
+    {id: '3', title: "Third"},
+    {id: '4', title: "Fourth"}, //can't have the same id for later
+  ]
+
+  const selectedList = (item: dataType) => {
+    setSelectedId(item.id);
+    console.log("selected" + item.title)
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +35,23 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will goe</Text>
+          <FlatList 
+            data = {DATA} //required
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({item}) => (
+              // code that i want to show for every item
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style = {[styles.titleContainer, 
+                  {
+                    backgroundColor: 
+                      item.id === selectedId ? colors.primary : colors.secondary,
+                  }
+                ]}>
+                  <Text style = {styles.titleText}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -32,6 +67,7 @@ const styles = StyleSheet.create({
     width: 300,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    backgroundColor: colors.primary,
   },
   titleText: {
     fontSize: 24,
